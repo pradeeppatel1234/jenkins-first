9b2b307eb1ba1c5790c0af58df6f6ab46f4097c8
pradeeppatel1234 <141856046+pradeeppatel1234@users.noreply.github.com>
2024-02-22 18:21:01 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+package javaAll;
+
+public class Main {
+	
+	public static void main(String args[]) {
+System.out.println("this is first program");
+System.out.println("this is second program");
+
+System.out.println("this is second program");
+
+System.out.println("this is second program");
+System.out.println("this is second program");
+System.out.println("this is second program");
+System.out.println("this is  program");
+}
+}
